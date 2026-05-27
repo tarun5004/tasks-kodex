29615240c8acecb83733360e043b1977415aa8f2
Tarun Raj Gaur <tarunraj7077@gmail.com>
2026-05-27 21:13:10 +0530

    add todo and user schema

---
+++ TODO-with-Auth/Attempt-1/src/models/todo.model.js
@@ -0,0 +1,16 @@
+import mongoose from 'mongoose';
+
+const todoSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: false,
+  },
+});
+
+const Todo = mongoose.model('Todo', todoSchema);
+
+export default Todo;
