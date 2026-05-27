9a80eaa2f897103d442c1bcc00d24ea46204db84
Tarun Raj Gaur <tarunraj7077@gmail.com>
2026-05-27 22:51:29 +0530

    start attempt 2 and create basic setup of express

---
+++ TODO-with-Auth/Attempt-2/server.js
@@ -0,0 +1,18 @@
+import app from './src/app.js';
+import dotenv from 'dotenv';
+dotenv.config();
+
+
+let PORT = process.env.PORT || 3000;
+
+let server = async () => {
+    try {
+        app.listen(PORT, () => {
+            console.log(`Server is running on port ${PORT}`);
+        });
+    } catch (error) {
+        console.error('Error starting server:', error);
+    }
+};
+
+server();
