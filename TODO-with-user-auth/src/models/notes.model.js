21ce82eb33559419c6ee12b68d745ded7b78dec7
Tarun Raj Gaur <tarunraj7077@gmail.com>
2026-05-30 01:16:37 +0530

    edit note schema

--- TODO-with-user-auth/src/models/notes.model.js
+++ TODO-with-user-auth/src/models/notes.model.js
@@ -1,19 +1,17 @@
-// src/models/notes.model.js
+// models/notes.model.js
 import mongoose from "mongoose";
 
 const noteSchema = new mongoose.Schema(
   {
     title: {
       type: String,
-      required: [true, "Note title is required"],
+      required: true,
       trim: true,
-      minlength: [3, "Title must be at least 3 characters"],
-      maxlength: [100, "Title cannot be more than 100 characters"],
     },
 
     content: {
       type: String,
-      required: [true, "Note content is required"],
+      required: true,
       trim: true,
     },
 
@@ -21,21 +19,6 @@ const noteSchema = new mongoose.Schema(
       type: mongoose.Schema.Types.ObjectId,
       ref: "User",
       required: true,
-    },
-
-    isPinned: {
-      type: Boolean,
-      default: false,
-    },
-
-    isArchived: {
-      type: Boolean,
-      default: false,
-    },
-
-    tags: {
-      type: [String],
-      default: [],
     },
   },
   {
